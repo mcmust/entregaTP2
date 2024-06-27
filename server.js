fb2e22b29d961cf8c422007af71b50666cfd67cb
mcmust <mcmustillo@gmail.com>
2024-06-27 20:16:58 -0300

    imports

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express')
+const path = require('path');
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mysql = require('mysql2')
 const db = require('./db')
 const routes = require('./routes');
 
+
 const app = express()
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static('public'))
+
 app.use(bodyParser.json())
 app.use(cors())
-app.use(express.static('public'))
 
 // Use the routes from routes.js
 app.use('/api', routes);
