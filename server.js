23fab3f5b81bcc936f33080c22dab80a30b25c73
mcmust <mcmustillo@gmail.com>
2024-06-27 20:25:08 -0300

    folder tree

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const routes = require('./routes');
 
 const app = express()
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.static('public'))
 
 app.use(bodyParser.json())
 app.use(cors())
